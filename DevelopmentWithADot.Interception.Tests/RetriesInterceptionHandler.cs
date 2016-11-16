3680ccaa6b78fa1776d521aa549230e305b53d70
Ricardo Peres <rjperes@hotmail.com>
2016-11-16 11:46:45 +0000

    Adding retries strategy

---
+++ DevelopmentWithADot.Interception.Tests/RetriesInterceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace DevelopmentWithADot.Interception.Tests
+{
+	public sealed class RetriesInterceptionHandler : IInterceptionHandler
+	{
+		public int Retries { get; set; }
+		public TimeSpan Delay { get; set; }
+
+		public void Invoke(InterceptionArgs arg)
+		{
+			for (var i = 0; i < this.Retries; i++)
+			{
+				try
+				{
+					arg.Proceed();
+					break;
+				}
+				catch
+				{
+					if (i != this.Retries - 1)
+					{
+						Thread.Sleep(this.Delay);
+					}
+					else
+					{
+						throw;
+					}
+				}
+			}
+		}
+	}
+}
